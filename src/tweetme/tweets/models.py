b749dc9adb010652be0fc406fc0cbb00a28a3c76
Prakash <prakashdas1998.pd@gmail.com>
2018-10-02 13:55:31 +0530

    Iniialzed app with views

---
+++ src/tweetme/tweets/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Tweets(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,default=1)
+    content     =   models.CharField(max_length=140)
+    updated  = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return str(self.content)
