dd668e6032e420e39f7cf342079f8057a029461b
Prakash <prakashdas1998.pd@gmail.com>
2018-10-02 17:42:18 +0530

    Added forms

--- src/tweetme/tweets/models.py
+++ src/tweetme/tweets/models.py
@@ -1,23 +1,22 @@
 from django.db import models
 from django.conf import settings
-from django.core.exceptions import ValidationError
-
+from .validators import validate_content
 # Create your models here.
 
-def validate_content(value):
-    content = value
-    if content == "abc":
-         raise ValidationError("Content cannot be abc")
-    return value
+
 
 class Tweets(models.Model):
     user        =   models.ForeignKey(settings.AUTH_USER_MODEL,default=1)
-    content     =   models.CharField(max_length=140)
+    content     =   models.CharField(max_length=140, validators=[validate_content])
     updated     =   models.DateTimeField(auto_now=True)
     timestamp   =   models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.content)
+
+    class Meta:
+        verbose_name = "Tweet"
+        verbose_name_plural = "Tweets"
 
     """ def clean(self,*args,**kwargs):
         content = self.content
