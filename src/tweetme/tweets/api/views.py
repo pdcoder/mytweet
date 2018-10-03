14c8bc71d179fe9bc8c886c5414328f70ad3d47a
Prakash <prakashdas1998.pd@gmail.com>
2018-10-03 22:43:48 +0530

    Added static files

--- src/tweetme/tweets/api/views.py
+++ src/tweetme/tweets/api/views.py
@@ -1,16 +1,12 @@
 from rest_framework import generics
+
+
+from tweets.models import Tweet
 from .serializers import TweetModelSerializer
-from tweets.models import Tweets
 
-class TweetCreateAPIView(generics.CreateAPIView):
-    serializer_class = TweetModelSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-    
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
 
     def get_queryset(self):
-        return Tweets.objects.all()
+        return Tweet.objects.all()
